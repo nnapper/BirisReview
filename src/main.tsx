8ab0434b2d9ee6e27669548b6901b6d5b9a4e2de
nnapper <alvinlyao@gmail.com>
2026-08-31 12:42:26 -0700

    Added the mantine notifications component

--- src/main.tsx
+++ src/main.tsx
@@ -8,19 +8,20 @@ import { RouterProvider } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import '@mantine/dates/styles.css'
+import '@mantine/notifications/styles.css'
 import './styles/index.css'
+import { Notifications } from '@mantine/notifications'
 const root = createRoot(document.getElementById('root') as HTMLElement)
 const routerInstance = router(store)
-
-// read data from localStorage
-// dispatch(updateSetting)
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <MantineProvider>
+        <Notifications />
         <RouterProvider router={routerInstance} />
       </MantineProvider>
     </Provider>
+    ,
   </React.StrictMode>,
 )
