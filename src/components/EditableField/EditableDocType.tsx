b26f98ba6525269d172c212327706d83c18aa5e5
nnapper <alvinlyao@gmail.com>
2026-09-03 14:09:22 -0700

    Condensed EditableDocType into one component to better keep track of internal states and fixed a bug where you would not be able to delete numbers

--- src/components/EditableField/EditableDocType.tsx
+++ src/components/EditableField/EditableDocType.tsx
@@ -1,33 +1,68 @@
 import { useState } from 'react'
-import { DocTypeSelection3 } from './DocTypeSelection'
-import { Button, Grid, GridCol } from '@mantine/core'
+import {
+  Button,
+  Grid,
+  GridCol,
+  Autocomplete,
+  type AutocompleteProps,
+  type ComboboxItem,
+  type OptionsFilter,
+} from '@mantine/core'
+import { useDocTypesQuery } from '../../features/doc/docApi'
 
 export const EditableDocType = ({
   value,
   label,
   onSave,
   onCancel,
-  validate,
 }: EditableFieldProps) => {
-  const [v, setV] = useState<number>(+value)
+  const initialDocTypeId = value
+  const [docTypeId, setDocTypeId] = useState<string>(value)
+  const [validEntry, setValidEntry] = useState<boolean>(false)
+
+  const { data: docTypesRaw, error } = useDocTypesQuery()
+  if (error || !docTypesRaw) return <div>Unable to pull doc types</div>
+
+  const docTypes = docTypesRaw.toSorted((a, b) => a.id - b.id)
+  const filter: OptionsFilter = ({ options, search }) => {
+    if (search == null || search === '') return options
+    const s = search.trim()
+    return (options as ComboboxItem[]).filter(option =>
+      option.label.startsWith(s),
+    )
+  }
+
+  const renderList: AutocompleteProps['renderOption'] = ({ option }) => {
+    const docType = docTypes.find(docType => docType.id === +option.value)
+    return docType ?
+        <div>
+          {docType.id} - {docType.dscr}
+        </div>
+      : option.value
+  }
 
   return (
     <Grid>
       <GridCol span={4}>{label}:</GridCol>
       <GridCol span={6}>
-        <DocTypeSelection3
-          docTypeId={+value}
-          onSelected={newVal => setV(newVal != null ? newVal.id : -1)}
+        <Autocomplete
+          defaultValue={initialDocTypeId}
+          value={docTypeId}
+          data={docTypes.map(docType => docType.id + '')}
+          onChange={value => {
+            setDocTypeId(value)
+            setValidEntry(
+              initialDocTypeId !== value &&
+                docTypes.find(docType => docType.id === +value) != null,
+            )
+          }}
+          renderOption={renderList}
+          filter={filter}
+          autoSelectOnBlur
         />
       </GridCol>
       <GridCol span={2}>
-        <Button
-          onClick={() => {
-            console.log('v is', v)
-            onSave(v + '')
-          }}
-          disabled={!validate(v + '')}
-        >
+        <Button onClick={() => onSave(docTypeId)} disabled={!validEntry}>
           Save
         </Button>
         <Button bg="#495057" onClick={onCancel}>
